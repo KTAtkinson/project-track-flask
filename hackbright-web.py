a6e78cbcd2028d3acb405e4631545a5c55020f87
Hackbright Student <info@hackbrightacademy.com>
2016-01-26 18:02:11 -0800

    Added functionality to add a student.

--- hackbright-web.py
+++ hackbright-web.py
@@ -24,6 +24,26 @@ def get_student_form():
     return render_template("student_search.html")
 
 
+@app.route("/student-add", methods=["post"])
+def student_add():
+    """Add a student."""
+
+    github = request.form.get("github")
+    first_name = request.form.get("first-name")
+    last_name = request.form.get("last-name")
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("user_added.html", first_name=first_name,
+                            last_name=last_name, github=github)
+
+
+@app.route("/student-add", methods=["get"])
+def get_student_creation_form():
+    """Show form for creating a student."""
+    
+    return render_template("student_creation_form.html")
+
+
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
     app.run(debug=True)
