edafb4376e63aa98acb8737c3436d595a5778e5e
Hackbright Student <info@hackbrightacademy.com>
2016-01-26 17:13:16 -0800

    Clean commit.

---
+++ hackbright-web.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = "jhacks"
+    first, last, github = hackbright.get_student_by_github(github)
+    return "%s is the GitHub account for %s %s" % (github, first, last)
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
