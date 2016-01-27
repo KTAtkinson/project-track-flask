604b4c89b160ccd1782d03179998f0c5c10a4eb7
Hackbright Student <info@hackbrightacademy.com>
2016-01-26 17:34:20 -0800

    Added templates for /student and /student-search.

--- hackbright-web.py
+++ hackbright-web.py
@@ -9,9 +9,20 @@ app = Flask(__name__)
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    return render_template('student_info.html', 
+                           first=first,
+                           last=last,
+                           github=github)
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
